da865b5b48731fd5e5164cae266f34553dd74cd3
Muhammad Tahir Iqbal <qazimtahir@gmail.com>
2024-02-16 20:28:05 +0500

    printingStart in TS

---
+++ printingStars.js
@@ -0,0 +1,8 @@
+"use strict";
+for (let i = 1; i <= 5; i++) {
+    let whiteSpace = " ";
+    for (let j = 1; j <= i; j++) {
+        whiteSpace = whiteSpace + " * ";
+    }
+    console.log(whiteSpace);
+}
---
+++ printingStarts.js
@@ -0,0 +1,6 @@
+"use strict";
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        console.log("*");
+    }
+}
